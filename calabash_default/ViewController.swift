f52b8b969a3ce3f64b2bfea09f89bca128396fca
Túlio Bazan da Silva <tucobz@gmail.com>
2016-08-02 23:16:05 -0300

    Add Clean Project

---
+++ calabash_default/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  calabash_default
+//
+//  Created by Túlio Bazan da Silva on 02/08/16.
+//  Copyright © 2016 TulioBZ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
